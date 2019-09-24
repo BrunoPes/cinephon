6c8fd8c89b0eaf185ce218c0a5b88e83cdc1ab00
Bruno Pessôa <brunoaraujopessoa@hotmail.com>
2019-09-24 03:36:18 -0300

    Theme colors file

---
+++ src/utils/Colors.js
@@ -0,0 +1,12 @@
+export default Colors = {
+  red: '#EC1D24',
+  dark: '#22262A',
+  darkGrey: '#6F6F6F',
+  darkGrey2: '#4F4F4F',
+  darkGrey3: '#818181',
+  lightGrey: '#AEAEAE',
+  lightGrey2: '#CECECE',
+  lightGrey3: '#EFEFEF',
+  white: '#FEFEFE',
+  yellow: '#E8B706',
+};
